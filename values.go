f39e53f2ce778d10c0e5a5b96352b66fc86c90a3
Vidhi.Panda <itn.vidhi.panda@grabpay.com>
2020-04-10 22:52:02 +0530

    Adding values.go

---
+++ values.go
@@ -0,0 +1,18 @@
+// Author: Vidhi Panda <vidhimpanda@gmail.com>
+// Description :Go values
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Go" + "Lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("11/3 =", 11/3)
+	fmt.Printf("11.0/3 = %0.2f\n", 11.0/3)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
