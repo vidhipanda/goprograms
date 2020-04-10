be8a606287d69f37ba702b005fe027bb6bac8df8
Vidhi.Panda <itn.vidhi.panda@grabpay.com>
2020-04-10 23:06:12 +0530

    Adding variables.go

---
+++ variables.go
@@ -0,0 +1,29 @@
+// Author: Vidhi Panda <vidhimpanda@gmail.com>
+// Description :Variables in Go
+
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	var s int
+	fmt.Println(s)
+
+	var fl float64
+	fmt.Println(fl)
+
+	f := "apple"
+	fmt.Println(f)
+}
